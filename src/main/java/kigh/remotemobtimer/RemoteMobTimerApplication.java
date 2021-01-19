d705b786d2c7d539c9af2aa3dc4ba62a6df26f6f
Kaiichiro Ota <kigh.ota@gmail.com>
2021-01-20 01:13:57 +0900

    initial commit

---
+++ src/main/java/kigh/remotemobtimer/RemoteMobTimerApplication.java
@@ -0,0 +1,13 @@
+package kigh.remotemobtimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteMobTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteMobTimerApplication.class, args);
+	}
+
+}
